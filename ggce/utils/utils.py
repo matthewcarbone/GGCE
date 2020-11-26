12c9210633664f6a78c75f08622b779f6e7a545a
Matthew Carbone <x94carbone@gmail.com>
2020-11-25 20:31:46 -0500

    Debug last line of requeue

--- ggce/utils/utils.py
+++ ggce/utils/utils.py
@@ -168,6 +168,7 @@ class SlurmWriter:
             for line in lines:
                 f.write(f"{line}\n")
             if self.cl_args['requeue']:
+                last_line = f"{last_line} &"
                 assert 'total_time' in list(self.loaded_config.keys())
                 assert 'time' in list(self.loaded_config.keys())
                 assert 'time_min' in list(self.loaded_config.keys())
