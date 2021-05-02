f8c83d89f1a57661c115ca0794fef154c7fffba6
Matthew Carbone <x94carbone@gmail.com>
2021-05-02 16:15:30 -0400

    Refactor, lint, cleanup API

--- ggce/executors/parallel.py
+++ ggce/executors/parallel.py
@@ -1,153 +1,79 @@
 #!/usr/bin/env python3
 
-__author__ = "Matthew R. Carbone & John Sous"
-__maintainer__ = "Matthew R. Carbone"
-__email__ = "x94carbone@gmail.com"
-
 import numpy as np
 import time
 
-from ggce.executors.base import BaseExecutor
-
 from petsc4py import PETSc
 
-BYTES_TO_MB = 1048576
+from ggce.executors.serial import SerialSparseExecutor
+from ggce.engine.physics import G0_k_omega
 
 
-class ParallelSparseExecutor(BaseExecutor):
+class ParallelSparseExecutor(SerialSparseExecutor):
     """A class to connect to PETSc powerful parallel sparse solver tools, to
     calculate G(k,w) in parallel."""
 
-    def _init_petsc(self, comm):
-        """Quick helper function inserts the MPI communicator into the class
-        scope and calculates the given process's rank."""
+    def _setup_petsc_structs(self):
+        """This function serves to initialize the various vectors and matrices
+        (using PETSc data types) that are needed to solve the linear problem.
+        They are setup using the sparse scheme, in parallel, so that each
+        process owns only a small chunk of it."""
 
-        self.comm = comm
-        self.rank = comm.getRank()
+        # Initialize the parallel vector b from Ax = b
+        self._vector_b = PETSc.Vec().create(comm=self.comm)
 
-    def prime(self, comm):
+        # Need to set the total size of the vector
+        self._vector_b.setSizes(self._linsys_size)
 
-        '''
-        Prepare the executor for running by finding the
-        system of equations and basis.
+        # This sets all the other PETSc options as defaults
+        self._vector_b.setFromOptions()
 
-        Parameters
-        ----------
-        comm : MPI_COMM_WORLD
-                Needed by PETSc to create the parallel matrices and vectors.
-                Pass specifically PETSc's COMM version for optimal results.
-        Returns
-        -------
+        # Now we create the solution vector, x in Ax = b
+        self._vector_x = self._vector_b.duplicate()
 
-        '''
+        # Now determine what is the local size PETSc picked
+        _n_local = self._vector_b.getLocalSize()
 
-        self._init_petsc(comm)
-        self._prime_parameters()
-        self._prime_system()
-        self._basis = self._system.get_basis(full_basis=True)
-        # get the total size of the linear system -- needed by PETSc
-        self.linsys_size = len(self._basis)
-        # call structs to initialize the PETSc vectors and matrices
+        # Figure out what the given process owns
+        self._rstart, self._rend = self._vector_b.getOwnershipRange()
+
+        # Create the matrix for the linear problem
+        self._mat_X = PETSc.Mat().create(comm=self.comm)
+
+        # set the matrix dimensions
+        # input format is [(n,N),(m,M)] where capitals are total matrix
+        # dimensions and lowercase are local block dimensions
+        # see bottom of PETSc listserv entry
+        # https://lists.mcs.anl.gov/mailman/htdig/petsc-users/2015-March/024879.html
+        # for example
+        self._mat_X.setSizes(
+            [(_n_local, self._linsys_size), (_n_local, self._linsys_size)]
+        )
+        # This sets all the other PETSc options as defaults
+        self._mat_X.setFromOptions()
+
+        # This is needed for some reason before PETSc matrix can be used
+        self._mat_X.setUp()
+
+    def prime(self):
+        """Prepare the executor for running by finding the system of equations
+        and basis. Requires a communicator be provided at instantiation."""
+
+        if self.comm is None:
+            self._logger.error("Prime failed, no MPI communicator provided")
+            return
+
+        self._sparse_prime_helper()
+
+        # Get the total size of the linear system -- needed by PETSc
+        self._linsys_size = len(self._basis)
+
+        # Call structs to initialize the PETSc vectors and matrices
         self._setup_petsc_structs()
 
-    def _setup_petsc_structs(self):
-
-        ''' This function serves to initialize the various vectors
-            and matrices (using PETSc data types) that are needed
-            to solve the linear problem. They are setup using the
-            sparse scheme, in parallel, so that each process owns
-            only a small chunk of it.
-
-        Parameters
-        ----------
-        Returns
-        -------
-            '''
-
-        # initialize the parallel vector b from Ax = b
-        self.v = PETSc.Vec().create(comm=self.comm)
-        # need to set the total size of the vector
-        self.v.setSizes(self.linsys_size)
-        # this sets all the other PETSc options as defaults
-        self.v.setFromOptions()
-        # now we create the solution vector, x in Ax = b
-        self.x = self.v.duplicate()
-        ## now determine what is the local size PETSc picked
-        self.nlocal = self.v.getLocalSize()
-        ## Now we need to figure out what the given process owns
-        self.rstart,self.rend = self.v.getOwnershipRange()
-
-        ## now create the matrix for the linear problem
-        self.X = PETSc.Mat().create(comm=self.comm)
-        ## set the matrix dimensions
-        ## input format is [(n,N),(m,M)] where capitals are total matrix
-        ## dimensions and lowercase are local block dimensions
-        ## see bottom of PETSc listserv entry
-        ## https://lists.mcs.anl.gov/mailman/htdig/petsc-users/2015-March/024879.html
-        ## for example
-        self.X.setSizes( [(self.nlocal,self.linsys_size),\
-                                    (self.nlocal,self.linsys_size)] )
-        # this sets all the other PETSc options as defaults
-        self.X.setFromOptions()
-        ## this is needed for some reason before PETSc matrix can be used
-        self.X.setUp()
-
-
-    def _matr_from_eqs(self, k, w, eta):
-
-        ''' This function iterates through the GGCE equations dicts
-            to extract the row, column coordiante and value of the
-            nonzero entries in the matrix. This is subsequently
-            used to construct the parallel sparse system matrix.
-            This is exactly the same as in the Serial class: however
-            that method returns X, v whereas here we need row_ind/col_ind_dat.
-
-        Parameters
-        ----------
-        k : float
-            The momentum quantum number point of the calculation.
-        w : float
-            The frequency grid point of the calculation.
-        eta : float, optional
-            The artificial broadening parameter of the calculation (the default
-            is None, which uses the value provided in parameter_dict at
-            instantiation).
-
-        Returns
-        -------
-        list, list, list
-            The row and column coordinate lists, as well as a list of
-            values of the matrix that are nonzero.
-        '''
-
-        row_ind = []
-        col_ind = []
-        dat = []
-
-        total_bosons = np.sum(self._parameters.N)
-        for n_bosons in range(total_bosons + 1):
-            for eq in self._system.equations[n_bosons]:
-                row_dict = dict()
-                index_term_id = eq.index_term.identifier()
-                ii_basis = self._basis[index_term_id]
-
-                for term in eq.terms_list + [eq.index_term]:
-                    jj = self._basis[term.identifier()]
-                    try:
-                        row_dict[jj] += term.coefficient(k, w, eta)
-                    except KeyError:
-                        row_dict[jj] = term.coefficient(k, w, eta)
-
-                row_ind.extend([ii_basis for _ in range(len(row_dict))])
-                col_ind.extend([key for key, _ in row_dict.items()])
-                dat.extend([value for _, value in row_dict.items()])
-
-        return row_ind, col_ind, dat
-
     def _assemble_matrix(self, k, w, eta):
-
-        ''' The function uses the GGCE equation sparse format data
-            to construct a sparse matrix in the PETSc scheme.
+        """The function uses the GGCE equation sparse format data to construct
+        a sparse matrix in the PETSc scheme.
 
         Parameters
         ----------
@@ -162,36 +88,42 @@ class ParallelSparseExecutor(BaseExecutor):
 
         Returns
         -------
-        The matrices self.X, self.v are constructed in-place,
+        The matrices self._mat_X, self._vector_b are constructed in-place,
         nothing is returned.
-        '''
+        """
 
-        row_ind, col_ind, dat = self._matr_from_eqs(k, w, eta)
+        row_ind, col_ind, dat = self._sparse_matrix_from_equations(k, w, eta)
+
+        t0 = time.time()
 
         row_start = np.zeros(1, dtype='i4')
-        col_pos = np.zeros(1,dtype='i4')
-        val = np.zeros(1,dtype='complex128')
-        loc_extent = range(self.rstart, self.rend)
-        for i, row_coo in enumerate(row_ind):
+        col_pos = np.zeros(1, dtype='i4')
+        val = np.zeros(1, dtype='complex128')
+        loc_extent = range(self._rstart, self._rend)
+        for ii, row_coo in enumerate(row_ind):
             if row_coo in loc_extent:
-                row_start, col_pos, val = row_coo, col_ind[i],dat[i]
-                self.X.setValues(row_start, col_pos, val)
+                row_start, col_pos, val = row_coo, col_ind[ii], dat[ii]
+                self._mat_X.setValues(row_start, col_pos, val)
 
-        ## assemble the matrix now that the values are filled in
-        self.X.assemblyBegin(self.X.AssemblyType.FINAL)
-        self.X.assemblyEnd(self.X.AssemblyType.FINAL)
+        # Assemble the matrix now that the values are filled in
+        self._mat_X.assemblyBegin(self._mat_X.AssemblyType.FINAL)
+        self._mat_X.assemblyEnd(self._mat_X.AssemblyType.FINAL)
 
-        ## let us go ahead and assign values for the b vector
-        self.v.setValues(self.linsys_size - 1,self._system.equations[0][0].bias(k,w))
-        ## need to assemble before use
-        self.v.assemblyBegin()
-        self.v.assemblyEnd()
+        # Assign values for the b vector
+        finfo = self._parameters.get_fFunctionInfo()
+        G0 = G0_k_omega(k, w, finfo.a, finfo.eta, finfo.t)
+        self._vector_b.setValues(self._linsys_size - 1, G0)
 
-        # check memory usage
-        ### to be implemented
+        # Need to assemble before use
+        self._vector_b.assemblyBegin()
+        self._vector_b.assemblyEnd()
 
-    def solve(self, k, w, eta=None):
+        # TODO: check memory usage
 
+        dt = time.time() - t0
+        self._logger.debug("PETSc matrix assembled", elapsed=dt)
+
+    def solve(self, k, w, eta=None, rtol=1.0e-15):
         """Solve the sparse-represented system using PETSc's KSP context.
 
         Parameters
@@ -204,6 +136,8 @@ class ParallelSparseExecutor(BaseExecutor):
             The artificial broadening parameter of the calculation (the default
             is None, which uses the value provided in parameter_dict at
             instantiation).
+        rtol : float, optional
+            PETSc's relative tolerance (the default is 1.0e-15).
 
         Returns
         -------
@@ -213,54 +147,51 @@ class ParallelSparseExecutor(BaseExecutor):
             using the PETSc KSP context.
         """
 
-
-        ## function call to construct the sparse matrix into self.X
+        # Function call to construct the sparse matrix into self._mat_X
         self._assemble_matrix(k, w, eta)
-        self._logger.debug(f"PETSc matrix assembled.")
 
-        ## now construct the desired solver instance
+        t0 = time.time()
+
+        # Now construct the desired solver instance
         ksp = PETSc.KSP().create()
-        ksp.setType('preonly') ##preonly for e.g. mumps and other external solvers
-        ## define the linear system matrix and its preconditioner
-        ksp.setOperators(self.X,self.X)
 
-        ## set preconditioner options
-        ## see PETSc manual for details
+        # "preonly" for e.g. mumps and other external solvers
+        ksp.setType('preonly')
+
+        # Define the linear system matrix and its preconditioner
+        ksp.setOperators(self._mat_X, self._mat_X)
+
+        # Set preconditioner options (see PETSc manual for details)
         pc = ksp.getPC()
         pc.setType('lu')
         pc.setFactorSolverType('mumps')
-        # set tolerance and options
-        ksp.setTolerances(rtol=1.e-15)
+
+        # Set tolerance and options
+        ksp.setTolerances(rtol=rtol)
         ksp.setFromOptions()
 
-        self._logger.debug(f"KSP and PC contexts initialized.")
+        dt = time.time() - t0
+        self._logger.debug("KSP and PC contexts initialized", elapsed=dt)
 
-        ## call the solve method
+        # Call the solve method
         t0 = time.time()
-        res = ksp.solve(self.v,self.x)
+        ksp.solve(self._vector_b, self._vector_x)
         dt = time.time() - t0
 
         self._logger.debug("Sparse matrices solved", elapsed=dt)
 
         # assemble the solution vector
-        self.x.assemblyBegin()
-        self.x.assemblyEnd()
+        self._vector_x.assemblyBegin()
+        self._vector_x.assemblyEnd()
 
         self.comm.barrier()
 
-        ## the last rank has the end of the solution vector, which contains G
-        ## G is the last entry
-        if self.rank == self.comm.getSize()-1: ## for future: change to rank 0
-            G = self.x.getValue(self.linsys_size-1)
+        # The last rank has the end of the solution vector, which contains G
+        # G is the last entry
+        if self.rank == 0:
+            G = self._vector_x.getValue(self._linsys_size-1)
             return np.array(G), {'time': [dt]}
 
-''' NEED TO FIGURE OUT HOW TO GATHER THE VECTOR INTO A SERIAL FORM USING PETSC MPI_COMM '''
-
-        # G = res[self._basis['{G}(0.0)']]
-        #
-        # if -G.imag / np.pi < 0.0:
-        #     self._logger.error(
-        #         f"Negative A({k:.02f}, {w:.02f}): {(-G.imag / np.pi):.02f}"
-        #     )
-
-''' END OF : NEED TO FIGURE OUT HOW TO BCAST THE VECTOR INTO A SERIAL FORM '''
+        # TODO: figure out how to gather the vector into serial form using
+        # the MPI communicator. I.e., return the entire resultant vector on
+        # rank 0, not just G.
--- ggce/executors/serial.py
+++ ggce/executors/serial.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python3
 
-__author__ = "Matthew R. Carbone & John Sous"
-__maintainer__ = "Matthew R. Carbone"
-__email__ = "x94carbone@gmail.com"
-
 import numpy as np
 from scipy import linalg
 from scipy.sparse import coo_matrix
@@ -20,10 +16,69 @@ BYTES_TO_MB = 1048576
 class SerialSparseExecutor(BaseExecutor):
     """Uses the SciPy sparse solver engine to solve for G(k, w) in serial."""
 
-    def prime(self):
+    def _sparse_prime_helper(self):
+        """Helper primer for use in the sparse solvers."""
+
         self._prime_parameters()
         self._prime_system()
         self._basis = self._system.get_basis(full_basis=True)
+
+    def prime(self):
+        self._sparse_prime_helper()
+
+    def _sparse_matrix_from_equations(self, k, w, eta):
+        """This function iterates through the GGCE equations dicts to extract
+        the row, column coordiante and value of the nonzero entries in the
+        matrix. This is subsequently used to construct the parallel sparse
+        system matrix. This is exactly the same as in the Serial class: however
+        that method returns X, v whereas here we need row_ind/col_ind_dat.
+
+        Parameters
+        ----------
+        k : float
+            The momentum quantum number point of the calculation.
+        w : float
+            The frequency grid point of the calculation.
+        eta : float, optional
+            The artificial broadening parameter of the calculation (the default
+            is None, which uses the value provided in parameter_dict at
+            instantiation).
+
+        Returns
+        -------
+        list, list, list
+            The row and column coordinate lists, as well as a list of values of
+            the matrix that are nonzero.
+        """
+
+        t0 = time.time()
+
+        row_ind = []
+        col_ind = []
+        dat = []
+
+        total_bosons = np.sum(self._parameters.N)
+        for n_bosons in range(total_bosons + 1):
+            for eq in self._system.equations[n_bosons]:
+                row_dict = dict()
+                index_term_id = eq.index_term.identifier()
+                ii_basis = self._basis[index_term_id]
+
+                for term in eq.terms_list + [eq.index_term]:
+                    jj = self._basis[term.identifier()]
+                    try:
+                        row_dict[jj] += term.coefficient(k, w, eta)
+                    except KeyError:
+                        row_dict[jj] = term.coefficient(k, w, eta)
+
+                row_ind.extend([ii_basis for _ in range(len(row_dict))])
+                col_ind.extend([key for key, _ in row_dict.items()])
+                dat.extend([value for _, value in row_dict.items()])
+
+        dt = time.time() - t0
+        self._logger.debug("Sparse matrix initialized", elapsed=dt)
+
+        return row_ind, col_ind, dat
 
     def _scaffold(self, k, w, eta=None):
         """Prepare the X, v sparse representation of the matrix to solve.
@@ -47,27 +102,7 @@ class SerialSparseExecutor(BaseExecutor):
 
         t0 = time.time()
 
-        row_ind = []
-        col_ind = []
-        dat = []
-
-        total_bosons = np.sum(self._parameters.N)
-        for n_phonons in range(total_bosons + 1):
-            for eq in self._system.equations[n_phonons]:
-                row_dict = dict()
-                index_term_id = eq.index_term.identifier()
-                ii_basis = self._basis[index_term_id]
-
-                for term in eq.terms_list + [eq.index_term]:
-                    jj = self._basis[term.identifier()]
-                    try:
-                        row_dict[jj] += term.coefficient(k, w, eta)
-                    except KeyError:
-                        row_dict[jj] = term.coefficient(k, w, eta)
-
-                row_ind.extend([ii_basis for _ in range(len(row_dict))])
-                col_ind.extend([key for key, _ in row_dict.items()])
-                dat.extend([value for _, value in row_dict.items()])
+        row_ind, col_ind, dat = self._sparse_matrix_from_equations(k, w, eta)
 
         X = coo_matrix((
             np.array(dat, dtype=np.complex64),
@@ -189,7 +224,7 @@ class SerialDenseExecutor(BaseExecutor):
         t0 = time.time()
         A = self._fill_matrix(k, w, n_phonons, 1, eta)
         dt = time.time() - t0
-        self._logger.debug("Filled beta in", elapsed=dt)
+        self._logger.debug("Filled beta", elapsed=dt)
         return A
 
     def solve(self, k, w, eta=None):
