56d399f57a8a31eff11347bb3f219de6c54dc283
Chiffafox <stepan.fomichev95@gmail.com>
2021-05-12 18:18:41 -0700

    updated PETSc benchmark to current usage added saving of G and exec times to disk

--- benchmarks/benchmark_petsc.py
+++ benchmarks/benchmark_petsc.py
@@ -1,48 +1,53 @@
 # before we import numpy, set the number of threads for numpy multithreading
 # typically most gains are achieved by 4-6 threads
-import os
-
-os.environ["MKL_NUM_THREADS"] = "6"
-os.environ["NUMEXPR_NUM_THREADS"] = "6"
-os.environ["OMP_NUM_THREADS"] = "6"
-
 import numpy as np
 
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-
-import petsc4py
-from petsc4py import PETSc
+# uncomment if want to visualize PETSc speed benchmark
+# import matplotlib as mpl
+# import matplotlib.pyplot as plt
 
 import sys
+import os
+
+script_dir = os.path.dirname(os.path.realpath(__file__))
+ggce_head_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 try:
     import ggce
 except ModuleNotFoundError:
-    sys.path.append("..")
+    sys.path.append(ggce_head_dir)
     import ggce
 
-from ggce.executors.serial import SerialDenseExecutor
-from ggce.executors.parallel import ParallelSparseExecutor
+from petsc4py import PETSc
 
-cloud_max = 10 # 8
-boson_max = 6 # 12
+from ggce.executors.serial import SerialDenseExecutor
+from ggce.executors.petsc4py.parallel import ParallelSparseExecutor
+
+# run PETSc benchmark at cloud size cloud_ext with up to bosons_max bosons
+cloud_ext = 10
+bosons_max = 8
+
 omega = 1.25
 coupling = 2.5
 hopping = 0.1
 eta = 0.005
+
+# pick specific point to do a calculation for speed benchmarking
 w = -5.5
 k = 0.5 * np.pi
 
-# we will loop over the range of allowed N_bosons to execute one computation (k,w)
+# loop over the range of allowed N_bosons to execute one computation (k,w)
 # start with PETSc
-comm = PETSc.COMM_WORLD
-parallel_times = np.zeros(boson_max)
-for n in range(1,boson_max+1):
+COMM = PETSc.COMM_WORLD
+
+parallel_times = np.zeros(bosons_max)
+parallel_greens = np.zeros(bosons_max, dtype='complex')
+
+for n in range(1,bosons_max+1):
 
     system_params = {
         "model": ["EFB"],
-        "M_extent": [cloud_max],
+        "M_extent": [cloud_ext],
         "N_bosons": [n],
         "Omega": [omega],
         "dimensionless_coupling": [coupling],
@@ -51,26 +56,24 @@ for n in range(1,boson_max+1):
         "protocol": "zero temperature"
     }
 
-    executor = ParallelSparseExecutor(system_params, "INFO")
-    executor.prime(comm)
+    executor = ParallelSparseExecutor(system_params, "info", mpi_comm=COMM)
+    executor.prime()
 
-    try:
-        G, time_dict = executor.solve(k, w) #'''use spectrum func'''
-    except TypeError:
-        continue
-    # print(f"this is time dict parallel {time_dict}")
-    parallel_times[n-1] = time_dict['time'][-1]
+    G, meta = executor.solve(k, w)
+    parallel_times[n-1] = meta['time'][-1]
+    parallel_greens[n-1] = G
 
 # now use the head node to do the serial calculation
-if comm.getRank() == 0:
+if COMM.getRank() == 0:
 
-    serial_times = np.zeros(boson_max)
+    serial_times = np.zeros(bosons_max)
+    serial_greens = np.zeros(bosons_max, dtype='complex')
     # do the same serially on the head node
-    for n in range(1,boson_max+1):
+    for n in range(1,bosons_max+1):
 
         system_params = {
             "model": ["EFB"],
-            "M_extent": [cloud_max],
+            "M_extent": [cloud_ext],
             "N_bosons": [n],
             "Omega": [omega],
             "dimensionless_coupling": [coupling],
@@ -79,22 +82,30 @@ if comm.getRank() == 0:
             "protocol": "zero temperature"
         }
 
-        executor = SerialDenseExecutor(system_params, "INFO")
+        executor = SerialDenseExecutor(system_params, "info")
         executor.prime()
 
-        G, time_dict = executor.solve(k, w) #'''use spectrum func'''
-        # print(f"this is time dict serial {time_dict}")
-        serial_times[n-1] = time_dict['time'][-1]
+        G, meta = executor.solve(k, w)
+        serial_times[n-1] = meta['time'][-1]
+        serial_greens[n-1] = G
 
-## now we plot the two timings to compare
-if comm.getRank() == 0:
-    plt.scatter(range(1,boson_max+1), \
-                serial_times, \
-                label = f"serial, dense (cont'd frac.), M = {cloud_max}")
-    plt.scatter(range(1,boson_max+1), \
-                parallel_times, \
-                label = f"parallel, sparse (PETSc), M = {cloud_max}")
-    plt.xlabel('N bosons')
-    plt.ylabel('CPU time, seconds')
-    plt.legend()
-    plt.savefig(f'benchmark_petsc_m{str(cloud_max)}_n{str(boson_max)}.png',format='png')
+## now we print to disk (and plot, if have matplotlib) the two timings to compare
+if COMM.getRank() == 0:
+
+    xx = np.array([serial_times, parallel_times, serial_greens, parallel_greens]).T
+    np.savetxt(os.path.join(script_dir,f"speed_benchmark_M_{str(cloud_ext)}_n_{str(bosons_max)}.txt"), xx,\
+                header = f"serial time (s)    parallel time (s)    serial G    parallel G",\
+                fmt = ['%.3f %+.0ej', '%.3f %+.0ej' ,'%.5e %+.5ej', '%.5e %+.5ej'],\
+                delimiter = "    " )
+
+    # uncomment if speed benchmark visualization desired
+    # plt.scatter(range(1,bosons_max+1), \
+    #             serial_times, \
+    #             label = f"serial, dense (cont'd frac.), M = {cloud_ext}")
+    # plt.scatter(range(1,bosons_max+1), \
+    #             parallel_times, \
+    #             label = f"parallel, sparse (PETSc), M = {cloud_ext}")
+    # plt.xlabel('N bosons')
+    # plt.ylabel('CPU time, seconds')
+    # plt.legend()
+    # plt.savefig(os.path.join(script_dir,f'benchmark_petsc_m{str(cloud_ext)}_n{str(bosons_max)}.png'),format='png')
