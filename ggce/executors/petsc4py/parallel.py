cf9ec8e54af9b4dcb6060cca63aa8d6994ea01a9
Chiffafox <stepan.fomichev95@gmail.com>
2021-05-21 16:11:00 -0700

    Re-wrote the executor structure to a) start from a base class, b) incorporate nnz preallocation, c) clean up residuals tracking and use the preconditioner for manual residual check, d) added the MUMPS and GMRES separate executor classes and appropriate tutorials. Slightly cleaned up the logging statements, changed the final solved message to appear alongside error check.

--- ggce/executors/petsc4py/parallel.py
+++ ggce/executors/petsc4py/parallel.py
@@ -5,127 +5,25 @@ import time
 
 from petsc4py import PETSc
 
-from ggce.executors.serial import SerialSparseExecutor
+from ggce.executors.petsc4py.base import BaseExecutorPETSC
 from ggce.engine.physics import G0_k_omega
 
 BYTES_TO_GB = 1073741274
 
 
-class ParallelSparseExecutor(SerialSparseExecutor):
+class ParallelSparseExecutorMUMPS(BaseExecutorPETSC):
     """A class to connect to PETSc powerful parallel sparse solver tools, to
-    calculate G(k,w) in parallel."""
+    calculate G(k,w) in parallel, using a one-shot sparse sovler MUMPS.
+    This inherits the matrix construction strategies of the BaseExecutorPETSC
+    base class.
 
-    def _setup_petsc_structs(self):
-        """This function serves to initialize the various vectors and matrices
-        (using PETSc data types) that are needed to solve the linear problem.
-        They are setup using the sparse scheme, in parallel, so that each
-        process owns only a small chunk of it."""
+    This is done because e.g. convergence and memory checks are often specific
+    to the particular solver used -- and so is the KSP context (i.e. solver)
+    setup."""
 
-        # Initialize the parallel vector b from Ax = b
-        self._vector_b = PETSc.Vec().create(comm=self.mpi_comm)
-
-        # Need to set the total size of the vector
-        self._vector_b.setSizes(self._linsys_size)
-
-        # This sets all the other PETSc options as defaults
-        self._vector_b.setFromOptions()
-
-        # Now we create the solution vector, x in Ax = b
-        self._vector_x = self._vector_b.duplicate()
-
-        # Now determine what is the local size PETSc picked
-        _n_local = self._vector_b.getLocalSize()
-
-        # Figure out what the given process owns
-        self._rstart, self._rend = self._vector_b.getOwnershipRange()
-
-        # Create the matrix for the linear problem
-        self._mat_X = PETSc.Mat().create(comm=self.mpi_comm)
-
-        # set the matrix dimensions
-        # input format is [(n,N),(m,M)] where capitals are total matrix
-        # dimensions and lowercase are local block dimensions
-        # see bottom of PETSc listserv entry
-        # https://lists.mcs.anl.gov/mailman/htdig/petsc-users/2015-March/024879.html
-        # for example
-        self._mat_X.setSizes(
-            [(_n_local, self._linsys_size), (_n_local, self._linsys_size)]
-        )
-        # This sets all the other PETSc options as defaults
-        self._mat_X.setFromOptions()
-
-        # This is needed for some reason before PETSc matrix can be used
-        self._mat_X.setUp()
-
-    def prime(self):
-        """Prepare the executor for running by finding the system of equations
-        and basis. Requires a communicator be provided at instantiation."""
-
-        if self.mpi_comm is None:
-            self._logger.error("Prime failed, no MPI communicator provided")
-            return
-
-        self._sparse_prime_helper()
-
-        # Get the total size of the linear system -- needed by PETSc
-        self._linsys_size = len(self._basis)
-
-        # Call structs to initialize the PETSc vectors and matrices
-        self._setup_petsc_structs()
-
-    def _assemble_matrix(self, k, w, eta):
-        """The function uses the GGCE equation sparse format data to construct
-        a sparse matrix in the PETSc scheme.
-
-        Parameters
-        ----------
-        k : float or array_like
-            The momentum quantum number point of the calculation.
-        w : float or array_like
-            The frequency grid point of the calculation.
-        eta : float
-            The artificial broadening parameter of the calculation.
-
-        Returns
-        -------
-        The matrices self._mat_X, self._vector_b are constructed in-place,
-        nothing is returned.
-        """
-
-        row_ind, col_ind, dat = self._sparse_matrix_from_equations(k, w, eta)
-
-        t0 = time.time()
-
-        row_start = np.zeros(1, dtype='i4')
-        col_pos = np.zeros(1, dtype='i4')
-        val = np.zeros(1, dtype='complex128')
-        loc_extent = range(self._rstart, self._rend)
-        for ii, row_coo in enumerate(row_ind):
-            if row_coo in loc_extent:
-                row_start, col_pos, val = row_coo, col_ind[ii], dat[ii]
-                self._mat_X.setValues(row_start, col_pos, val)
-
-        # Assemble the matrix now that the values are filled in
-        self._mat_X.assemblyBegin(self._mat_X.AssemblyType.FINAL)
-        self._mat_X.assemblyEnd(self._mat_X.AssemblyType.FINAL)
-
-        # Assign values for the b vector
-        finfo = self._model.get_fFunctionInfo()
-        G0 = G0_k_omega(k, w, finfo.a, eta, finfo.t)
-        self._vector_b.setValues(self._linsys_size - 1, G0)
-
-        # Need to assemble before use
-        self._vector_b.assemblyBegin()
-        self._vector_b.assemblyEnd()
-
-        # TODO: check memory usage
-
-        dt = time.time() - t0
-        self._logger.debug("PETSc matrix assembled", elapsed=dt)
-
-    def check_convergence(self, factored_mat, rtol):
+    def check_conv(self, factored_mat, rtol, elapsed):
         """This helper function checks MUMPS convergence using built-in MUMPS
-        error codes and a manual residual check.
+        error codes. Factored_mat holds the MUMPS error codes and control values.
 
         Parameters
         ----------
@@ -136,47 +34,32 @@ class ParallelSparseExecutor(SerialSparseExecutor):
 
         Returns
         -------
-        The residual check and MUMPS convergenc criterions are conducted
-        in place, nothing is returned.
+        The MUMPS convergence criterion is conducted in place, nothing is
+        returned.
         """
 
         # MUMPS main convergence index -- if 0, all good
         mumps_conv_ind = factored_mat.getMumpsInfog(1)
-        if mumps_conv_ind == 0:
-            self._logger.debug(
-                "According to MUMPS diagnostics, call to MUMPS was "
-                "successful."
-            )
-        elif mumps_conv_ind < 0:
-            self._logger.error(
-                "A MUMPS error occured with MUMPS error code "
-                f"{mumps_conv_ind} See the MUMPS User Guide, Sec. 8, for "
-                "error  diagnostics."
-            )
-        elif mumps_conv_ind > 0:
-            self._logger.warning(
-                "A MUMPS warning occured with MUMPS warning code "
-                f"{mumps_conv_ind} See the MUMPS User Guide, Sec. 8, for "
-                "error diagnostics."
-            )
 
-        # Unhappy with MUMPS, we do our own double-check of residual criterion
-        res = self._vector_b - self._mat_X(self._vector_x)
-        res_norm = res.norm(PETSc.NormType.NORM_2)
+        # do the MUMPS check on the head node
         if self.mpi_rank == 0:
-            self._logger.debug(
-                f"MUMPS final residual is {res_norm}, rtol is {rtol}"
-            )
-
-        if res_norm > rtol:
-            self._logger.warning(
-                "Solution failed residual relative tolerance check. "
-                "Solutions likely not fully converged: "
-                f"res_norm ({res_norm:.02e}) > rtol ({rtol:.02e})"
-            )
-
-        else:
-            self._logger.debug("Solution passed manual residual check.")
+            if mumps_conv_ind == 0:
+                self._logger.debug(
+                    "According to MUMPS diagnostics, call to MUMPS was "
+                    f"successful. The calculation took {elapsed:.2f} sec."
+                )
+            elif mumps_conv_ind < 0:
+                self._logger.error(
+                    "A MUMPS error occured with MUMPS error code "
+                    f"{mumps_conv_ind} See the MUMPS User Guide, Sec. 8, for "
+                    f"error  diagnostics. The calculation took {elapsed:.2f} sec."
+                )
+            elif mumps_conv_ind > 0:
+                self._logger.warning(
+                    "A MUMPS warning occured with MUMPS warning code "
+                    f"{mumps_conv_ind} See the MUMPS User Guide, Sec. 8, for "
+                    f"error diagnostics. The calculation took {elapsed:.2f} sec."
+                )
 
     def check_mem_use(self, factored_mat):
         """This helper function checks MUMPS memory usage with built-in MUMPS
@@ -200,7 +83,7 @@ class ParallelSparseExecutor(SerialSparseExecutor):
             f"Current rank MUMPS memory usage is {rank_mem_used:.02f} GB"
         )
 
-        # set up memory usage tracking, report to the logger
+        # set up memory usage tracking, report to the logger on head node only
         if self.mpi_rank == 0:
             # total memory across all processes
             total_mem_used = factored_mat.getMumpsInfog(31) * 1e6 / BYTES_TO_GB
@@ -208,7 +91,7 @@ class ParallelSparseExecutor(SerialSparseExecutor):
                 f"Total MUMPS memory usage is {total_mem_used:.02f} GB"
             )
 
-    def solve(self, k, w, eta, rtol=1.0e-15):
+    def solve(self, k, w, eta, rtol=1.0e-10):
         """Solve the sparse-represented system using PETSc's KSP context.
         Note that this method only returns values on MPI rank = 0. All other
         ranks will return None.
@@ -222,7 +105,7 @@ class ParallelSparseExecutor(SerialSparseExecutor):
         eta : float
             The artificial broadening parameter of the calculation.
         rtol : float, optional
-            PETSc's relative tolerance (the default is 1.0e-15).
+            PETSc's relative tolerance (the default is 1.0e-10).
 
         Returns
         -------
@@ -263,7 +146,10 @@ class ParallelSparseExecutor(SerialSparseExecutor):
         ksp.solve(self._vector_b, self._vector_x)
         dt = time.time() - t0
 
-        self._logger.debug("Sparse matrices solved", elapsed=dt)
+        ## export some data from the solver and return on debug
+        # solver_info = ksp.view()
+        # if self.mpi_rank == 0:
+        #     self._logger.debug(solver_info)
 
         # assemble the solution vector
         self._vector_x.assemblyBegin()
@@ -271,14 +157,179 @@ class ParallelSparseExecutor(SerialSparseExecutor):
 
         self.mpi_comm.barrier()
 
-        # implement manual residual check, as well as check MUMPS INFO
+        # call manual residual check, as well as check MUMPS INFO
         # if the MUMPS solver call was successful
         # to get access to MUMPS error codes, get the factored matrix from PC
         factored_mat = pc.getFactorMatrix()
-        self.check_convergence(factored_mat, rtol=rtol)
+        self.check_conv_manual(pc, rtol=rtol)
+        self.check_conv(factored_mat, rtol=rtol, elapsed=dt)
 
         # now check memory usage
         self.check_mem_use(factored_mat)
+
+        self.mpi_comm.barrier()
+
+        # The last rank has the end of the solution vector, which contains G
+        # G is the last entry aka "the last equation" of the matrix
+        # use a gather operation, called by all ranks, to construct the full
+        # vector
+        G = self.mpi_comm.gather(self._vector_x.getArray(), root=0)
+        # this returns a list of values of G from all processes
+
+        if self.mpi_rank == 0:
+            # now select only the final process list and final value
+            G = G[self.mpi_world_size-1][-1]
+            return np.array(G), {'time': [dt]}
+
+class ParallelSparseExecutorGMRES(BaseExecutorPETSC):
+    """A class to connect to PETSc powerful parallel sparse solver tools, to
+    calculate G(k,w) in parallel, using an iterative solver GMRES.
+    This inherits the matrix construction strategies of the BaseExecutorPETSC
+    base class.
+
+    This is done because e.g. convergence and memory checks are often specific
+    to the particular solver used -- and so is the KSP context (i.e. solver)
+    setup."""
+
+    def check_conv(self, ksp, rtol, elapsed):
+        """This helper function checks GMRES convergence using built-in PETSc
+        error codes.
+
+        Parameters
+        ----------
+        ksp : PETSc_KSP
+            The Krylov Subspace solution context that contains convergence
+            codes, convergence history (i.e. residuals from iterations)
+            and more.
+
+        Returns
+        -------
+        The residual check and PETSc convergence criterions are conducted
+        in place, nothing is returned.
+        """
+
+        # GMRES main convergence index
+        gmres_conv_ind = ksp.getConvergedReason()
+        if self.mpi_rank == 0:
+            if gmres_conv_ind > 0:
+                self._logger.debug(
+                    f"According to PETSc diagnostics, call to GMRES was "
+                    f"successful. It exited with code {gmres_conv_ind}."
+                    f"See the PETSc header in petsc/include/petscksp.h, "
+                    f"lines 518-680 for details. The calculation took {elapsed:.2f} sec."
+                )
+            elif gmres_conv_ind < 0:
+                self._logger.error(
+                    "A PETSc calculation divergence was detected with error code  "
+                    f"{gmres_conv_ind}. See include/petscksp.h, lines 518-680 for "
+                    f"error  diagnostics. The calculation took {elapsed:.2f} sec."
+                )
+
+        # now do a check using the final residual from getconvergenceHistory
+        res_hist = ksp.getConvergenceHistory()
+        its = ksp.getIterationNumber()
+        # if on head node, log the residual history for debugging
+        if self.mpi_rank == 0:
+            self._logger.debug(f"Run ended after {its} iterations. "
+                                    f"Convergence history is \n {res_hist}.")
+
+    def check_mem_use(self, factored_mat):
+        """This helper function checks PETSc sovler memory usage
+        access.
+
+        Parameters
+        ----------
+        factored_mat : PETSc_Mat
+            The factorized linear system matrix from the PETSc'
+            pre-condictioning context PC, obtained after the
+            solver has been called.
+
+        Returns
+        -------
+        The memory resuts are given to the logger, nothing is returned.
+
+        WARNING: unfortunately PETSc python wrapper does not have wrapping for
+        memory stuff.
+        """
+
+        raise NotImplementedError
+
+    def solve(self, k, w, eta, rtol=1.0e-10):
+        """Solve the sparse-represented system using PETSc's KSP context.
+        Note that this method only returns values on MPI rank = 0. All other
+        ranks will return None.
+
+        Parameters
+        ----------
+        k : float
+            The momentum quantum number point of the calculation.
+        w : float
+            The frequency grid point of the calculation.
+        eta : float
+            The artificial broadening parameter of the calculation.
+        rtol : float, optional
+            PETSc's relative tolerance (the default is 1.0e-10).
+
+        Returns
+        -------
+        np.ndarray, dict
+            The value of G and meta information, which in this case, is only
+            specifically the time elapsed to solve for this (k, w) point
+            using the PETSc KSP context.
+        """
+
+        # Function call to construct the sparse matrix into self._mat_X
+        self._assemble_matrix(k, w, eta)
+
+        t0 = time.time()
+
+        # Now construct the desired solver instance
+        ksp = PETSc.KSP().create()
+
+        # setting type of the solver to GMRES
+        ksp.setType('gmres')
+
+        # Define the linear system matrix and its preconditioner
+        ksp.setOperators(self._mat_X, self._mat_X)
+
+        # Set preconditioner options (see PETSc manual for details)
+        # is not required for GMRES, set by default to block Jacobi
+        # we still extract PC context for preconditioner access
+        pc = ksp.getPC()
+
+        # Set tolerance and remaining options from command line (if any)
+        ksp.setTolerances(rtol=rtol)
+        ksp.setFromOptions()
+        # Make a call to set up arrays for residual tracking
+        ksp.setConvergenceHistory()
+
+        dt = time.time() - t0
+        self._logger.debug("KSP context initialized", elapsed=dt)
+
+        # Call the solve method
+        t0 = time.time()
+        ksp.solve(self._vector_b, self._vector_x)
+        dt = time.time() - t0
+
+        ## export some data from the solver and return on debug
+        # solver_info = ksp.view()
+        # if self.mpi_rank == 0:
+        #     self._logger.debug(solver_info)
+
+        # assemble the solution vector
+        self._vector_x.assemblyBegin()
+        self._vector_x.assemblyEnd()
+
+        self.mpi_comm.barrier()
+
+        # implement manual residual check, as well as check PETSc output code
+        # in there we call for convergence history
+        self.check_conv_manual(pc, rtol=rtol)
+        self.check_conv(ksp, rtol=rtol, elapsed=dt)
+
+        # now check memory usage
+        # memory check not wrapped in petsc4py for GMRES
+        # self.check_mem_use(factored_mat)
 
         self.mpi_comm.barrier()
 
