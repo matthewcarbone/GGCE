772579d17ad3f1d9e843146aaa7e54ab33ff840c
Matthew Carbone <x94carbone@gmail.com>
2021-07-30 19:38:45 -0400

    Add more tests to combinatorics

--- tests/utils/test_combinatorics.py
+++ tests/utils/test_combinatorics.py
@@ -1,5 +1,7 @@
 import pytest
 
+from scipy.special import comb
+
 from ggce.utils import combinatorics
 
 
@@ -13,3 +15,18 @@ def test_generalized_equations_combinatorics_term_is1(mn):
 def test_generalized_equations_combinatorics_term():
     assert combinatorics.generalized_equations_combinatorics_term(3, 3) == 3
     assert combinatorics.generalized_equations_combinatorics_term(2, 3) == 2
+
+
+@pytest.mark.parametrize("M,N", [(10, 4), (4, 10), (5, 5), (2, 8)])
+def test_total_generalized_equations(M, N):
+    s = 0
+    for m in range(1, M + 1):
+        for n in range(1, N + 1):
+            s += combinatorics.generalized_equations_combinatorics_term(m, n)
+    assert s == combinatorics.total_generalized_equations(M, N)
+
+
+@pytest.mark.parametrize("lt", [(10, 4), (4, 10), (5, 5), (2, 8)])
+def test_config_space_gen(lt):
+    c = comb(lt[0] + lt[1] - 1, lt[1], exact=True)
+    assert len(list(combinatorics.config_space_gen(*lt))) == c
