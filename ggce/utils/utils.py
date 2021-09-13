c4af3ba26c5e1593cbdbb8adb9b68b44e2172873
Matthew Carbone <x94carbone@gmail.com>
2021-09-12 20:09:30 -0400

    Add Metadata class to utils

--- ggce/utils/utils.py
+++ ggce/utils/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import json
 from pathlib import Path
 import pickle
 import uuid
@@ -25,6 +26,47 @@ class Buffer:
         self.counter += 1
         if self.counter >= self.nbuff:
             self.flush()
+
+
+class Metadata(dict):
+
+    @classmethod
+    def load(cls, path, ok_not_exist=True):
+        """Loads in a metadata json object from disk.
+        
+        Parameters
+        ----------
+        path : str
+            Path to the json file in question.
+        ok_not_exist : bool, optional
+            If True, then if the file path does not exist, this fact will be
+            silently ignored and an "empty" metadata file will be returned.
+            Default is True.
+        
+        Returns
+        -------
+        Metadata
+            The metadata class.
+        """
+
+        path = Path(path)
+
+        if not path.exists():
+            return cls(dict(), path=path)
+
+        with open(path, "r") as infile:
+            d = json.load(infile)
+        return cls(d, path=path)
+
+    def save(self):
+        """Saves the state of the dictionary to the user-provided path."""
+
+        with open(self._path, "w") as outfile:
+            json.dump(dict(self), outfile, indent=4, sort_keys=True)
+
+    def __init__(self, d=dict(), path=None):
+        super().__init__(d)
+        self._path = path
 
 
 def chunk_jobs(jobs, world_size, rank):
