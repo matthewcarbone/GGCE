127c37a2fc2aba1d8610d400845825e703a8cea9
Stepan Fomichev <stepan.fomichev95@gmail.com>
2022-11-24 20:28:10 -0500

    added petsc import to simplify import structure

--- ggce/__init__.py
+++ ggce/__init__.py
@@ -2,6 +2,7 @@ from .logger import logger  # noqa
 from .model import Model  # noqa
 from .engine.system import System  # noqa
 from .executors.solvers import SparseSolver, DenseSolver  # noqa
+from .executors.petsc4py.solvers import MassSolverMUMPS  # noqa
 
 
 # DO NOT CHANGE ANYTHING BELOW THIS
