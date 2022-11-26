80929ba6728ff062a9c64803b6d517bb61e74461
Stepan Fomichev <stepan.fomichev95@gmail.com>
2022-11-26 17:53:03 -0500

    change to conform to black package formatter checks

--- ggce/__init__.py
+++ ggce/__init__.py
@@ -2,6 +2,7 @@ from .logger import logger  # noqa
 from .model import Model  # noqa
 from .engine.system import System  # noqa
 from .executors.solvers import SparseSolver, DenseSolver  # noqa
+
 try:
     from .executors.petsc4py.solvers import MassSolverMUMPS  # noqa
 except ImportError:
